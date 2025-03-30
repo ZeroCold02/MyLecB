a85cb610e24ba5f92248cdb560b339c64c823384
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-03-30 22:28:19 +0900

    Feat: 스위치 기능 구현

--- app/src/main/java/com/example/mylecb/uicomponents/MainScreen.kt
+++ app/src/main/java/com/example/mylecb/uicomponents/MainScreen.kt
@@ -2,12 +2,15 @@ package com.example.mylecb.uicomponents
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mylecb.model.TodoItemFactory
+import com.example.week03.lab.model.TodoStatus
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
@@ -16,9 +19,21 @@ fun MainScreen(modifier: Modifier = Modifier) {
         TodoItemFactory.makeTodoList() //리스트 만들어둔거 받아오는거
     }
 
+    var onlyShowIncomplete by remember { mutableStateOf(false) }
+
     Column { // 또는 fillMaxHeight()) {
         TodoListTitle()
-        TodoList(todoList)
+        TodoSwitch(
+            isChecked = onlyShowIncomplete,
+            onCheckedChange = { onlyShowIncomplete = it } // 이게 핵심!
+        )
+        val filteredList = if (onlyShowIncomplete) {
+            todoList.filter { it.status == TodoStatus.PENDING }.toMutableList()
+        } else {
+            todoList
+        }
+
+        TodoList(filteredList)
         Spacer(modifier = Modifier.weight(1f))
         TodoItemInput(todoList)
     }
