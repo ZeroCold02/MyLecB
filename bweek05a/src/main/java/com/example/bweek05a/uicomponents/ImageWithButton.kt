dd7d4712e4197e2cc12cf1a9b8753520e25d3cd1
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-01 13:08:53 +0900

    CHORE: 이미지와 버튼 넣기

--- bweek05a/src/main/java/com/example/bweek05a/uicomponents/ImageWithButton.kt
+++ bweek05a/src/main/java/com/example/bweek05a/uicomponents/ImageWithButton.kt
@@ -1,11 +1,29 @@
 package com.example.bweek05a.uicomponents
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 
 @Composable
-fun ImageWithButton(image:Int, modifier: Modifier = Modifier,
-                    button:@Composable ()->Unit
+fun ImageWithButton(
+    image: Int, modifier: Modifier = Modifier,
+    button: @Composable () -> Unit
 ) {
-    
+    Column {
+        AsyncImage(
+            model = image,
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+        )
+        button()
+    }
 }
