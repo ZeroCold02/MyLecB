43f2624227d6df9e52e3174abd1f73b90178dfb5
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-03 12:51:36 +0900

    Feat: saver 기본 구조
    Any로 저장해서 ImageData로 복원하겠다는거고
    save와 restore에 해당 기능을 구현해서 넘겨주면 됨.

--- bweek05a/src/main/java/com/example/bweek05a/model/ImageData.kt
+++ bweek05a/src/main/java/com/example/bweek05a/model/ImageData.kt
@@ -1,8 +1,17 @@
 package com.example.bweek05a.model
 
+import androidx.compose.runtime.saveable.listSaver
+
 data class ImageData (
     val imageUri: ImageUri,
     val buttonType: ButtonType = ButtonType.ICON,
     var likes:Int = 0,
     var dislikes:Int = 0
-)
+) {
+    companion object{
+        val imageSaver = listSaver<ImageData, Any>(
+            save = {},
+            restore = {}
+        )
+    }
+}
