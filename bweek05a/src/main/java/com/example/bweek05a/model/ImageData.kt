1cd829251b4be49ce50a3ed22fe179eacad5f181
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-03 12:57:46 +0900

    CHORE: save 함수 채우기

--- bweek05a/src/main/java/com/example/bweek05a/model/ImageData.kt
+++ bweek05a/src/main/java/com/example/bweek05a/model/ImageData.kt
@@ -10,8 +10,22 @@ data class ImageData (
 ) {
     companion object{
         val imageSaver = listSaver<ImageData, Any>(
-            save = {},
+            //imageUri는 기본 데이터 타입이 아니니까, save할때 기본 구조로 쪼개줘야겠지.?
+            //buttonType은 enum 클래스잖아. enum클래스는 그냥 저장할 수 있나봄 (name 을 사용해서 문자열로 저장)
+            save = {item->
+                listOf(
+                    when(item.imageUri) {
+                        is ImageUri.ResImage -> item.imageUri.resID
+                        is ImageUri.WebImage -> item.imageUri.urlStr
+                    },
+                    item.buttonType.name,
+                    item.likes,
+                    item.dislikes
+                )
+            },
             restore = {}
         )
     }
+
+
 }
