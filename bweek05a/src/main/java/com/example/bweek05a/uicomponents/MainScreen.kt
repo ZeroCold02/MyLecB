25e6ea71c20d14db4de8bcdd13437a213b5418c9
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-03 13:12:28 +0900

    CHORE: 초기값 설정
    이렇게한다고 저장이된다? -> ㄴㄴ
    Saver가 있어야 저장 및 복원이 된다.

--- bweek05a/src/main/java/com/example/bweek05a/uicomponents/MainScreen.kt
+++ bweek05a/src/main/java/com/example/bweek05a/uicomponents/MainScreen.kt
@@ -4,17 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import com.example.bweek05a.R
+import com.example.bweek05a.model.ButtonType
 import com.example.bweek05a.model.ImageData
+import com.example.bweek05a.model.ImageUri
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     val img1 = rememberSaveable {
         mutableStateOf(
             ImageData(
-                imageUri = TODO(),
-                buttonType = TODO(),
-                likes = TODO(),
-                dislikes = TODO()
+                imageUri = ImageUri.ResImage(R.drawable.img1),
+                buttonType = ButtonType.EMOJI,
+                likes = 50,
+                dislikes = 10
             )
         )
     }
