7068cb3c9325e9330b31da97c0781f3395135182
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-08 12:58:35 +0900

    CHORE: 뷰모델 클래스 생성
    viewmodel 패키지 생성
    ImageViewModel 클래스 생성
    ViewModel 상속

---
+++ bweek05a/src/main/java/com/example/bweek05a/viewmodel/ImageViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.bweek05a.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.bweek05a.model.ImageData
+import com.example.bweek05a.model.ImageListFactory
+
+class ImageViewModel: ViewModel() {
+    private val _imageList = ImageListFactory.makeImageList()
+    val imageList: MutableList<ImageData>
+        get() = _imageList
+}
