262d03b5f03d2a728d82045f883f5467731dba35
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-08 13:08:42 +0900

    Feat: ImageList
    버튼타입별 객체는
    MainScreen에 만들어뒀던거 복붙해서 수정.
    차이점은 item을 변경할 수 없고 imageList[index]로 접근한다는거?

--- bweek05a/src/main/java/com/example/bweek05a/uicomponents/ImageList.kt
+++ bweek05a/src/main/java/com/example/bweek05a/uicomponents/ImageList.kt
@@ -2,8 +2,32 @@ package com.example.bweek05a.uicomponents
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.bweek05a.model.ButtonType
+import com.example.bweek05a.model.ImageData
 
 @Composable
-fun ImageList(modifier: Modifier = Modifier) {
-
+fun ImageList(
+    imageList: MutableList<ImageData>,
+    modifier: Modifier = Modifier
+) {
+    imageList.forEachIndexed { index, item ->
+        when (item.buttonType) {
+            ButtonType.ICON -> {}
+            ButtonType.BADGE -> {}
+            ButtonType.EMOJI -> {
+                ImageWithButton(image = item.imageUri) {
+                    ButtonWithEmoji(
+                        likes = item.likes,
+                        dislikes = item.dislikes,
+                        onClickLikes = {
+                            imageList[index] = item.copy(likes = item.likes + 1)
+                        },
+                        onClickDisLikes = {
+                            imageList[index] = item.copy(dislikes = item.dislikes + 1)
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
