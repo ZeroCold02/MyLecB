cc8d878a2d0cd2450fe2794394c2e6aa46f548ca
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-08 13:15:32 +0900

    CHORE: 컴포넌트 삽입

--- bweek05a/src/main/java/com/example/bweek05a/uicomponents/MainScreen.kt
+++ bweek05a/src/main/java/com/example/bweek05a/uicomponents/MainScreen.kt
@@ -1,7 +1,12 @@
 package com.example.bweek05a.uicomponents
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -17,9 +22,15 @@ fun MainScreen(
     val orientation = LocalConfiguration.current.orientation
 
     if(orientation == Configuration.ORIENTATION_PORTRAIT) {
-        
+        Column(modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            ImageList(imageList)
+        }
     } else {
-
+        Row(modifier = Modifier.fillMaxHeight(),
+            verticalAlignment = Alignment.CenterVertically) {
+            ImageList(imageList)
+        }
     }
 
 }
