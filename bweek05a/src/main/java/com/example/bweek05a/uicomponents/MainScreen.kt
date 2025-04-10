4c8c993f92449e4590f6e23c26748ece3695c785
ColdZero <50104854+ZeroCold02@users.noreply.github.com>
2025-04-10 13:03:04 +0900

    CHORE: 실습 정정

--- bweek05a/src/main/java/com/example/bweek05a/uicomponents/MainScreen.kt
+++ bweek05a/src/main/java/com/example/bweek05a/uicomponents/MainScreen.kt
@@ -1,10 +1,13 @@
 package com.example.bweek05a.uicomponents
 
 import android.content.res.Configuration
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,19 +20,35 @@ fun MainScreen(
     modifier: Modifier = Modifier,
     imageViewModel: ImageViewModel = viewModel()
 ) {
+//    val imageList = imageViewModel.imageList
+//
+//    val orientation = LocalConfiguration.current.orientation
+//
+//    if(orientation == Configuration.ORIENTATION_PORTRAIT) {
+//        Column(modifier = Modifier.fillMaxWidth(),
+//            horizontalAlignment = Alignment.CenterHorizontally) {
+//            ImageList(imageList)
+//        }
+//    } else {
+//        Row(modifier = Modifier.fillMaxHeight(),
+//            verticalAlignment = Alignment.CenterVertically) {
+//            ImageList(imageList)
+//        }
+//    }
     val imageList = imageViewModel.imageList
-
     val orientation = LocalConfiguration.current.orientation
-
-    if(orientation == Configuration.ORIENTATION_PORTRAIT) {
-        Column(modifier = Modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally) {
-            ImageList(imageList)
+    val scrollState = rememberScrollState()
+    if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Column(
+            modifier = Modifier.fillMaxWidth().verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ImageList(imageList = imageList)
         }
-    } else {
-        Row(modifier = Modifier.fillMaxHeight(),
-            verticalAlignment = Alignment.CenterVertically) {
-            ImageList(imageList)
+    }else{
+        Row(modifier = Modifier.fillMaxHeight().horizontalScroll(scrollState),
+            verticalAlignment = Alignment.CenterVertically){
+            ImageList(imageList = imageList)
         }
     }
 
